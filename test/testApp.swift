a2efea4bc52eb131482ceb424b319dc3dbc4bf93
Frank <yunfan-shi@hotmail.com>
2021-07-24 20:45:50 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Frank on 2021/7/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
